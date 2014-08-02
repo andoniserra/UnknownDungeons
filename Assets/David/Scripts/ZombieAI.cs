e6e7a51aa2b99e456c4b20248c2515a150d9ef38
Razgriz85 <elchicoconinsomnio@gmail.com>
2014-08-02 23:58:45 +0200

    Mis scripts de movimiento pixelado
    Subo los scripts con los que he estado probando lo del movimiento, para
    que veáis lo que digo. Lo importante está en "Movement".
    El CharacterController2D no es mío, es un script que me bajé porque
    comprueba las colisiones mejor, sin él el sprite se me agitaba un huevo
    en cuanto te intentabas mover contra una pared.
    Ya de paso pues ya subo el proyecto de Unity creado ;)

---
+++ Assets/David/Scripts/ZombieAI.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Veeeery basic Zombie AI, just walks towards the player
+/// </summary>
+public class ZombieAI : MonoBehaviour 
+{
+	// The target (player) to pursue
+	public Transform m_victim;
+
+	// (debug) Target's stored position
+	public Vector3 m_target;
+
+	// (debug) Zombie's position
+	public Vector3 m_position;
+
+	// (debug) Movement vector to target's position
+	public Vector3 m_movement;
+	
+	// Movement Component to call in order to move the sprite
+	private Movement m_moveScript;
+
+	
+	void Awake() 
+	{
+		m_moveScript = this.GetComponent<Movement>();
+	}
+	
+
+	void Update () 
+	{
+		if (m_victim)
+		{
+			// Get player's position and steer towards it
+			m_target = m_victim.transform.position;
+			m_position = this.transform.position;
+
+			m_movement = m_target - m_position;
+
+			// Normalized to get a Vector of magnitude 1 similar to player's input values
+			Vector2 movement2D = new Vector2(m_movement.x,m_movement.y).normalized;
+
+			// Call the move
+			if (!(movement2D.Equals(Vector2.zero)))
+			{
+				m_moveScript.Move(movement2D);
+			}
+		}	
+	}
+}
