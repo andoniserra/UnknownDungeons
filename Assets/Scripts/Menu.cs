db7102e4f7bb2e6a041f735dc01ec0692d9ee567
Serrano Lomas, Andoni <aserrano011@ikasle.ehu.es>
2014-08-06 02:16:21 +0200

    Creación de las funciones para cajas y textos

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		/*
+		GameObject kek = new GameObject();
+		kek.AddComponent(typeof(SpriteRenderer));
+		SpriteRenderer imagen2 = kek.GetComponent<SpriteRenderer>();
+		imagen2.sprite = Simbolos.Cat;
+		imagen2.sortingLayerName = "Background";
+		imagen2.transform.position = Simbolos.colocar8x8(2,17);
+		*/
+
+		//Simbolos.caja(new Vector2(0,0), new Vector2(19,2));
+		//Simbolos.print("abcdefghijklmnopqrstuvwxyz0123456789!?", new Vector2(0,17));
+
+		Simbolos.caja(new Vector2 (0,0), new Vector2(19,2),"hola caracola!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/*
+		Debug.Log("herpaderpa");
+		Sprite[] letras = Resources.LoadAll<Sprite>("Sprites/tipografia");
+
+		foreach( Sprite sp in letras){
+			Debug.Log(sp.name);
+		}
+		Debug.Log("herpadorp");
+	 */
+}
