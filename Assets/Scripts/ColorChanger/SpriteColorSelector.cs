f3e3cb76f2e0fb2a2def85fb0bca86e4fb478ad5
Razgriz <elchicoconinsomnio@gmail.com>
2014-08-07 23:01:09 +0200

    Gráficos reimportados en BN y assets reorganizados
    - Imágenes pasadas a blanco y negro.
    - He sacado todos mis assets que están más o menos terminados de mi
    personal a sus correspondientes.

---
+++ Assets/Scripts/ColorChanger/SpriteColorSelector.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[ExecuteInEditMode]
+public class SpriteColorSelector : MonoBehaviour
+{
+	public Color [] colors = {new Color(1,0,0), new Color(0,1,0), new Color(0,0,1), 
+		new Color(1,1,0), new Color(0,1,1), new Color(1,0,1), new Color(1,1,1)};
+
+	public Color m_sceneColor;
+
+	public bool m_fixedColor = false;
+
+	private int m_index = -1;
+
+
+	public void RandomColor()
+	{
+		scs.m_sceneColor = colors[Random.Range (0, colors.Length)];
+		print(scs.m_sceneColor);
+	}
+
+
+	public void NextColor()
+	{
+		m_index += 1;
+		if ( m_index >= colors.Length )
+		{
+			m_index = 0;
+		}
+		scs.m_sceneColor = colors[m_index];
+		print(scs.m_sceneColor);
+	}
+
+
+	public static SpriteColorSelector scs;
+	
+	
+	// Al cargar el script comprobamos si ya existe una instancia
+	void Awake () 
+	{
+		// Si no existe
+		if (scs == null)
+		{
+			// Marcamos el objeto para no ser destruido al cambiar de escena
+			DontDestroyOnLoad(gameObject);
+			// Asignamos esta instancia a la instancia unica Singleton
+			scs = this;
+		}
+		// Si ya existia
+		else if (scs != this)
+		{
+			// Destruimos este objeto
+			Destroy(gameObject);
+		}
+	}
+}
